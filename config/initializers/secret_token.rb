97cc1b5f024e5bf7da57368488435e6eca423dbb
gartBart <zwirdsch@yahoo.de>
2010-09-09 16:41:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoccerManager::Application.config.secret_token = '41c2789800adaa87f0d22e6923898289592523f231e0f7127a1ed174b255b3f3291530e702b6fa3b0c9eace2396e83194aac43dec1a1c879881a6f8b840056ca'
