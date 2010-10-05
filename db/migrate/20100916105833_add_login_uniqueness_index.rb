09fe3e79e911c52c329c5256b546040408982a72
gartBart <zwirdsch@yahoo.de>
2010-10-05 14:56:20 +0200

    Finished first cut of the User model

--- db/migrate/20100916105833_add_login_uniqueness_index.rb
+++ db/migrate/20100916105833_add_login_uniqueness_index.rb
@@ -1,7 +1,9 @@
 class AddLoginUniquenessIndex < ActiveRecord::Migration
   def self.up
+    add_index :users, :login, :unique => true
   end
 
   def self.down
+    remove_index :users, :login
   end
 end
